fc6a3e92d4c507194cf6e9c3f3bab6cdb17809b2
rappire <rappire>
2022-11-29 17:36:27 +0900

    GalleryImage

---
+++ src/components/GalleryPage/GalleryImage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul>
+      <li v-for = "item in itemlist" v-bind:key="item">
+        <!-- <img src={{item}} alt="err"> -->
+        {{item}}
+      </li>
+    </ul>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      itemlist :["@/assets/GalleryPage/image1", "image2"]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/GalleryPage.vue
+++ src/views/GalleryPage.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <PageHeader></PageHeader>
+        <GalleryPage></GalleryPage>
     </div>
 </template>
 
 <script>
 import PageHeader from '@/components/PageHeader.vue';
+import GalleryPage from '@/components/GalleryPage/GalleryImage.vue';
 
 export default {
     components: {
         PageHeader,
+        GalleryPage,
     },
 }
 </script>
